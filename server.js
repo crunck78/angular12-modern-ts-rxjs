7e5db8ad6bf13be26b9abbe4aefc2bade1a93db5
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2022-07-09 21:40:16 +0200

    Adding JSON server with Node.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const DIR = './uploads';
+
+app.use(express.static(DIR));
+const PORT = 3500;
+app.listen(PORT, () => console.log('Server started '+ PORT) );
